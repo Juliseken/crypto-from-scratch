d6bf8308a836afcfa913aa78234e869ab0e179f0
Julian <r-julian@hotmail.de>
2026-01-22 01:16:03 +0100

    refactor: tests use built-in methods as oracles

--- src/test/java/de/juliseken/scytale/math/NumberTheoryTest.java
+++ src/test/java/de/juliseken/scytale/math/NumberTheoryTest.java
@@ -9,11 +9,17 @@ public class NumberTheoryTest {
 
     @Test
     public void testGcd() {
-        assertEquals(BigInteger.valueOf(6), NumberTheory.gcd(
-                BigInteger.valueOf(54), BigInteger.valueOf(24)));
-        assertEquals(BigInteger.ONE, NumberTheory.gcd(
-                BigInteger.valueOf(17), BigInteger.valueOf(13)));
-        assertEquals(BigInteger.valueOf(10), NumberTheory.gcd(
-                BigInteger.valueOf(0), BigInteger.valueOf(10)));
+        assertEquals(
+                NumberTheory.gcd(BigInteger.valueOf(54), BigInteger.valueOf(24)),
+                BigInteger.valueOf(54).gcd(BigInteger.valueOf(24))
+        );
+        assertEquals(
+                NumberTheory.gcd(BigInteger.valueOf(54), BigInteger.valueOf(24)),
+                BigInteger.valueOf(54).gcd(BigInteger.valueOf(24))
+        );
+        assertEquals(
+                NumberTheory.gcd(BigInteger.valueOf(0), BigInteger.valueOf(10)),
+                BigInteger.valueOf(0).gcd(BigInteger.valueOf(10))
+        );
     }
 }
