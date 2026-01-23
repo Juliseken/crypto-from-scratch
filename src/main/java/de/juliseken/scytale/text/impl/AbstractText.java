b002c4754f2cb3650f7b0595a1e4d5c4257fabcb
Julian <r-julian@hotmail.de>
2026-01-23 16:57:07 +0100

    feat: implement toString to be able to print messages

--- src/main/java/de/juliseken/scytale/text/impl/AbstractText.java
+++ src/main/java/de/juliseken/scytale/text/impl/AbstractText.java
@@ -16,6 +16,11 @@ public abstract class AbstractText implements de.juliseken.scytale.text.api.Text
     }
 
     @Override
+    public String toString() {
+        return content.toString();
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
